5ebfdcb8c94f0c16095b67852c90d785819678ca
Kath Pobee-Norris <kpobeenorris@gmail.com>
2017-03-14 13:39:07 +0000

    Created subjects controller and updated routes to be resourceful

---
+++ app/controllers/subjects_controller.rb
@@ -0,0 +1,27 @@
+class SubjectsController < ApplicationController
+  
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def created
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+  def destroy
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,24 @@
 Rails.application.routes.draw do
 
   root 'demo#index'
-  get 'demo/index'
-  get 'demo/hello'
-  get 'demo/other_hello'
-  get 'demo/lynda'
+
+  resources :subjects do
+    member do
+      get :delete
+    end
+  end
+
+  resources :pages do
+    member do
+      get :delete
+    end
+  end
+
+  resources :sections do
+    member do
+      get :delete
+    end
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
