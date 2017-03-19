0bd9c35cf2aabd4cef05112f078cca25d35eac02
Kath Pobee-Norris <kpobeenorris@gmail.com>
2017-03-19 10:13:11 +0000

    Added assest pipeline so all pages now include css

--- config/routes.rb
+++ config/routes.rb
@@ -20,5 +20,7 @@ Rails.application.routes.draw do
     end
   end
 
+  get 'demo/escape_output'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
