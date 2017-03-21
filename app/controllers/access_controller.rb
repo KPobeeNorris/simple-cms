c491502178a4824a96b154d9f516e43053d8883c
Kath Pobee-Norris <kpobeenorris@gmail.com>
2017-03-21 10:16:53 +0000

    Added #logout and #attempt_login as well as HTML for the views.

--- app/controllers/access_controller.rb
+++ app/controllers/access_controller.rb
@@ -1,7 +1,34 @@
 class AccessController < ApplicationController
+
+  layout 'admin'
+
   def menu
   end
 
   def login
   end
+
+  def attempt_login
+    if params[:username].present? && params[:password].present?
+      found_user = AdminUser.where(:username == params[:username]).first
+      if found_user
+        authorised_user = found_user.authenticate(params[:password])
+      end
+    end
+    if authorised_user
+      session[:user_id] = authorised_user.id
+      flash[:notice] = "You are now logged in"
+      redirect_to(admin_path)
+    else
+      flash.now[:notice] = "Invalid username/password combination."
+      render('login')
+    end
+  end
+
+  def logout
+    session[:user_id] = nil
+    flash[:notice] = "User logged out"
+    redirect_to(access_login_path)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   root 'demo#index'
 
+  get 'admin', :to => 'access#menu'
   get 'access/menu'
   get 'access/login'
   post 'access/attempt_login'
